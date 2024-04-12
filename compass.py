ba1ed6bb9ccbbdd10d73b9ea33895643fa678c9a
jboydRgu <115725995+jboydRgu@users.noreply.github.com>
2024-04-12 12:04:52 +0100

    completed door sensor

--- compass.py
+++ compass.py
@@ -2,13 +2,30 @@ from sense_hat import SenseHat
 import math
 import RPi.GPIO as GPIO
 import time
+from time import sleep
 
-BuzzerPin = 4
+BUZZER = 4
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BuzzerPin, GPIO.OUT) 
+GPIO.setup(BUZZER, GPIO.OUT) 
 
+def buzz(noteFreq, duration):
+    halveWaveTime = 1 / (noteFreq * 2 )
+    waves = int(duration * noteFreq)
+    for i in range(waves):
+       GPIO.output(BUZZER, True)
+       time.sleep(halveWaveTime)
+       GPIO.output(BUZZER, False)
+       time.sleep(halveWaveTime)
+def play():
+    t=0
+    notes=[262,294,330,262]
+    duration=[0.5,0.5,0.5,0.5]
+    for n in notes:
+        buzz(n, duration[t])
+        time.sleep(duration[t] *0.1)
+        t+=1
 
 field_strength = 0
 sense = SenseHat()
@@ -25,36 +42,4 @@ while  field_strength < 130:
   
 else:
     print("Door open")
-global Buzz
-
-Buzz = GPIO.PWM(BuzzerPin, 4) 
-Buzz.start(40) 
-
-C4=262
-D4=294
-E4=330
-F4=349
-G4=392
-A4=440
-B4=494
-
-song = [
-  G4,
-  E4, F4, G4,
-  A4, B4, 
-  D4, F4,
-  C4
-]
-
-beat = [
-  4,
-  4, 4, 2,
-  4, 4,
-  2, 1,
-  1
-]
-
-while True:
-	for i in range(1, len(song)): 
-		Buzz.ChangeFrequency(song[i]) 
-		time.sleep(beat[i]*0.13)
+play()
